53a48e5c4d59f51471b8e4645b34c4827c7f76a7
Breno Ramalho Lemes <breno@br-lemes.net>
2025-12-28 21:45:14 -0400

    feat: adicionar estrutura e primeiro exemplo
    * Adicionar licença BSD Zero Clause
    * Iniciar módulo Go
    * Criar primeiro exemplo "Hello, World"
    * Adicionar teste para o primeiro exemplo

---
+++ go-by-example/01-hello-world/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestMainOutput(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute program: %v", err)
+	}
+
+	expected := "Hello, World!\n"
+
+	got := string(output)
+	if got != expected {
+		t.Errorf("Unexpected output.\nExpected: %q\nGot: %q", expected, got)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/br-lemes/go-playground
+
+go 1.25.5
